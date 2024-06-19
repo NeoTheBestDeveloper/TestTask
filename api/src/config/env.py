3b66deccb8029126ec4168a7b63fa71764a8956e
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2024-06-19 11:02:09 +0300

    ci: create basic project structure

---
+++ api/src/config/env.py
@@ -0,0 +1,34 @@
+from os import getenv
+
+__all__ = [
+    "UndefinedEnvError",
+    "Env",
+    "EnvList",
+]
+
+
+class UndefinedEnvError(Exception):
+    """Raise when cannot read env variable and getenv return None."""
+
+    def __init__(self, env_name: str) -> None:
+        msg = f'Env name="{env_name}"'
+        super().__init__(msg)
+
+
+def Env[T](env_name: str, variable_type: type[T]) -> T:
+    env = getenv(env_name, None)
+
+    if env is None:
+        raise UndefinedEnvError(env_name)
+
+    return variable_type(env)
+
+
+def EnvList[T](env_name: str, list_item_type: type[T]) -> list[T]:
+    env = getenv(env_name, None)
+
+    if env is None:
+        raise UndefinedEnvError(env_name)
+
+
+    return map(list_item_type, env.split())
