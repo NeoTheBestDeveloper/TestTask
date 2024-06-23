67bbcf6c24737ec392b6a4e4dd33e336b351ef3d
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2024-06-23 15:22:33 +0300

    feat: add validation for login and registration forms

--- frontend/src/pages/auth/Login.vue
+++ frontend/src/pages/auth/Login.vue
@@ -1,38 +1,52 @@
 <script setup>
+import { ref } from 'vue';
+
 import { loginAPI } from '@/api/Auth';
 import { routerPush } from '@/router';
 import { useAuthStore } from '@/store/AuthStore';
-import { storeToRefs } from 'pinia';
-import { ref } from 'vue';
 
 const login = ref("");
 const password = ref("");
+const errorMsg = ref("");
 const valid = ref(false);
 
 const store = useAuthStore();
-const { isAuthenticated } = storeToRefs(store);
 
 const loginUser = async () => {
-    try {
-        await loginAPI(login.value, password.value);
+  if (!valid.value) {
+    return;
+  }
+  try {
+      await loginAPI(login.value, password.value);
+      login.value = "";
+      password.value = "";
 
-        login.value = "";
-        password.value = "";
-
-        isAuthenticated.value = true;
-        routerPush("main");
-    } catch (error) {
-        console.log(error);
-    }
+      store.isAuthenticated = true;
+      routerPush("main");
+  } catch (error) {
+    errorMsg.value = error.response.data.detail;
+  }
 }
+
+const loginRules = [
+  v => !!v || 'Логин обязателен',
+  v => (v && v.length <= 140) || 'Логин должен быть короче 140 символов',
+  v => (v && v.length >= 3) || 'Логин должен быть длиной хотя бы 3 символа',
+];
+
+const passwordRules = [
+  v => !!v || 'Пароль обязателен',
+  v => (v && v.length <= 255) || 'Пароль должен быть короче 256 символов',
+  v => (v && v.length >= 8) || 'Пароль должен быть длиной хотя бы 8 символов',
+];
 </script>
 
 <template>
   <v-form v-model="valid" v-on:submit.prevent="loginUser" fluid class="fill-height d-flex" >
     <v-row align="center" justify="center">
       <v-sheet  ms="auto" width="20%">
-        <v-text-field v-model="login" label="Логин" hide-details required ></v-text-field>
-        <v-text-field v-model="password" label="Пароль" type="password" hide-details required></v-text-field>
+        <v-text-field v-model="login" label="Логин" :rules="loginRules" :error-messages="errorMsg"></v-text-field>
+        <v-text-field v-model="password" label="Пароль" type="password" :rules="passwordRules" :error-messages="errorMsg"></v-text-field>
         <v-btn class="mt-2" type="submit" block>Войти</v-btn>
         <RouterLink class="blue-link" to="/register">Нет аккаунта? Зарегистрироваться</RouterLink>
       </v-sheet>
--- frontend/src/pages/auth/Registration.vue
+++ frontend/src/pages/auth/Registration.vue
@@ -8,29 +8,49 @@ const login = ref("");
 const email = ref("");
 const password = ref("");
 const valid = ref(false);
+const errorMsg = ref("");
+
 
 const registerUser = async () =>  {
     try {
-        const response = await registerAPI(login.value, email.value, password.value);
+      await registerAPI(login.value, email.value, password.value);
 
-        login.value = "";
-        email.value = "";
-        password.value = "";
+      login.value = "";
+      email.value = "";
+      password.value = "";
 
-        routerPush("login");
+      routerPush("login");
     } catch (error) {
-        console.log(error);
+      errorMsg.value = error.response.data.data.username;
     }
 }
+
+const loginRules = [
+  v => !!v || 'Логин обязателен',
+  v => (v && v.length <= 140) || 'Логин должен быть короче 140 символов',
+  v => (v && v.length >= 3) || 'Логин должен быть длиной хотя бы 3 символа',
+];
+
+const emailRules = [
+  v => !!v || 'Почта обязательна',
+  v => /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()\\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v) || "Невалидная почта",
+];
+
+const passwordRules = [
+  v => !!v || 'Пароль обязателен',
+  v => (v && v.length <= 255) || 'Пароль должен быть короче 256 символов',
+  v => (v && v.length >= 8) || 'Пароль должен быть длиной хотя бы 8 символов',
+];
+
 </script>
 
 <template>
 <v-form v-model="valid" v-on:submit.prevent="registerUser" fluid class="fill-height d-flex" >
     <v-row align="center" justify="center">
       <v-sheet  ms="auto" width="20%">
-        <v-text-field v-model="login" label="Логин" hide-details required></v-text-field>
-        <v-text-field v-model="email" label="Почта" hide-details required type="email"></v-text-field>
-        <v-text-field v-model="password" label="Пароль" type="password" hide-details required></v-text-field>
+        <v-text-field v-model="login" label="Логин" :rules="loginRules" :error-messages="errorMsg"></v-text-field>
+        <v-text-field v-model="email" label="Почта" type="email" :rules="emailRules"></v-text-field>
+        <v-text-field v-model="password" label="Пароль" type="password" :rules="passwordRules"></v-text-field>
         <v-btn class="mt-2" type="submit" block>Зарегистрироваться</v-btn>
         <RouterLink class="blue-link" to="/login">Уже есть аккаунт? Войти</RouterLink>
       </v-sheet>
